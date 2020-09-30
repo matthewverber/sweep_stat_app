46602df15097e25f6eb6fdf48ad870384678f681
Braden Goodwin <6876787+BradenGoodwin@users.noreply.github.com>
2020-09-29 20:54:54 -0400

    Updated deprecated 'title' field to 'label' in bottomNavBar items

--- lib/guided_setup.dart
+++ lib/guided_setup.dart
@@ -7,27 +7,19 @@ import 'guided_setup_pages/gs_page3.dart';
 void main() {
   runApp(MyApp());
 }
+
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'SweepStat',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-        textTheme: TextTheme(
-          bodyText1: TextStyle(
-            fontSize: 17.0,
-            height: 1.3
-          ),
-          bodyText2: TextStyle(
-            fontSize: 14.0,
-            height: 1.25
-          )
-        )
-      ),
-      home: GuidedSetup()
-    );
+        title: 'SweepStat',
+        theme: ThemeData(
+            primarySwatch: Colors.blue,
+            visualDensity: VisualDensity.adaptivePlatformDensity,
+            textTheme: TextTheme(
+                bodyText1: TextStyle(fontSize: 17.0, height: 1.3),
+                bodyText2: TextStyle(fontSize: 14.0, height: 1.25))),
+        home: GuidedSetup());
   }
 }
 
@@ -39,63 +31,59 @@ class GuidedSetup extends StatefulWidget {
 
 class _GuidedSetupState extends State<GuidedSetup> {
   int _currentPage = 0;
-  String selected = ">25"; // Temporary variable for holding selected value of page 3 until we implement the Voltammetry Settings
+  String selected =
+      ">25"; // Temporary variable for holding selected value of page 3 until we implement the Voltammetry Settings
   List<Widget> _pages;
 
-  void _onBottomNavTapped(int index){
-    if (index == 0 && _currentPage > 0){
+  void _onBottomNavTapped(int index) {
+    if (index == 0 && _currentPage > 0) {
       setState(() {
         _currentPage -= 1;
       });
-    } else if (index == 1 && _currentPage < 3){
-      setState((){
+    } else if (index == 1 && _currentPage < 3) {
+      setState(() {
         _currentPage += 1;
       });
     }
-
   }
 
   @override
-  void initState(){
+  void initState() {
     super.initState();
     _pages = [
       GSPage1(),
       GSPage2(),
-      GSPage3(selected: selected,
-        callback: (String val){
-          setState((){
-            selected = val;
-          });
-        }
-      )
+      GSPage3(
+          selected: selected,
+          callback: (String val) {
+            setState(() {
+              selected = val;
+            });
+          })
     ];
   }
 
-
-
-
   @override
   Widget build(BuildContext context) {
-    
     return Scaffold(
-      appBar: AppBar(title: Text('Guided Setup - Step ' + (_currentPage+1).toString() + " of 10")),
+      appBar: AppBar(
+          title: Text('Guided Setup - Step ' +
+              (_currentPage + 1).toString() +
+              " of 10")),
       body: Center(
-        child: FractionallySizedBox(
-          widthFactor: 0.9,
-          child: _pages[_currentPage]
-        )
-      ),
+          child: FractionallySizedBox(
+              widthFactor: 0.9, child: _pages[_currentPage])),
       bottomNavigationBar: BottomNavigationBar(
         showSelectedLabels: false,
         showUnselectedLabels: false,
         items: <BottomNavigationBarItem>[
           BottomNavigationBarItem(
             icon: Icon(Icons.chevron_left),
-            title: Text("")
+            label: "",
           ),
           BottomNavigationBarItem(
             icon: Icon(Icons.chevron_right),
-            title: Text("")
+            label: "",
           )
         ],
         onTap: _onBottomNavTapped,
@@ -103,5 +91,4 @@ class _GuidedSetupState extends State<GuidedSetup> {
       ),
     );
   }
-  
 }
