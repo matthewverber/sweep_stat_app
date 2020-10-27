7b2dfcb15bd76e5e6b59e37480e5639b914b0310
ByrdOfAFeather <matthew_a_byrd@outlook.com>
2020-10-26 21:32:58 -0400

    Bluetooth Screen Implemented

--- lib/bluetooth_mangement.dart
+++ lib/bluetooth_mangement.dart
@@ -1,9 +1,15 @@
+import 'dart:async';
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_blue/flutter_blue.dart';
 
 FlutterBlue flutterBlue = FlutterBlue.instance;
 
 class BlueToothSelection extends StatefulWidget {
+  /*
+   * Source : https://github.com/pauldemarco/flutter_blue/blob/master/example/lib/main.dart
+   */
   @override
   State<StatefulWidget> createState() {
     return _BlueToothSelectionState();
@@ -12,22 +18,55 @@ class BlueToothSelection extends StatefulWidget {
 
 class _BlueToothSelectionState extends State<BlueToothSelection> {
   @override
-  void initState() {
-    flutterBlue.startScan(timeout: Duration(seconds: 4));
-
-    var subscription = flutterBlue.scanResults.listen((results) {
-      // do something with scan results
-      for (ScanResult r in results) {
-        print('${r.device.name} found! rssi: ${r.rssi}');
-      }
-    });
-
-    flutterBlue.stopScan();
-    super.initState();
-  }
-
-  @override
   Widget build(BuildContext context) {
-    return null;
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Find Devices'),
+      ),
+      body: RefreshIndicator(
+        onRefresh: () => FlutterBlue.instance.startScan(timeout: Duration(seconds: 4)),
+        child: SingleChildScrollView(
+          child: Column(
+            children: <Widget>[
+              StreamBuilder<List<ScanResult>>(
+                stream: FlutterBlue.instance.scanResults,
+                initialData: [],
+                builder: (c, snapshot) => Column(
+                  children: snapshot.data.map((r) {
+                    if (r.device.name.isNotEmpty) {
+                      return Card(
+                        child: ListTile(
+                          title: Text(r.device.name),
+                          onTap: () {
+
+                          },
+                        ),
+                      );
+                    } else {
+                      return Container();
+                    }
+                  }).toList(),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+      floatingActionButton: StreamBuilder<bool>(
+        stream: FlutterBlue.instance.isScanning,
+        initialData: false,
+        builder: (c, snapshot) {
+          if (snapshot.data) {
+            return FloatingActionButton(
+              child: Icon(Icons.stop),
+              onPressed: () => FlutterBlue.instance.stopScan(),
+              backgroundColor: Colors.red,
+            );
+          } else {
+            return FloatingActionButton(child: Icon(Icons.search), onPressed: () => FlutterBlue.instance.startScan(timeout: Duration(seconds: 4)));
+          }
+        },
+      ),
+    );
   }
 }
